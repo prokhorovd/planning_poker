07e04ca09bd031239dbe2dbaae90f65f16e64875
prokhorovd <github@prokhorov.dev>
2022-02-06 15:00:17 +0400

    feat: add dotenv package on server-side for port management

--- server/app.tsx
+++ server/app.tsx
@@ -1,5 +1,8 @@
+require('dotenv').config();
+
 const express = require('express')
 const app = express()
+console.log(process.env.PORT);
 const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
