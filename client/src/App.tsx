cc7ed263a65baae7926ecc43f103ee179f2282b3
prokhorovd <github@prokhorov.dev>
2022-02-09 23:22:19 +0400

    feat: implement basic layout and header component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import ServerTest from './components/ServerTest';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to react template blank page.
-        </p>
-        <ServerTest />
-      </header>
+      <Header />
     </div>
-  );
+  )
 }
 
 export default App;
