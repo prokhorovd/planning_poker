5d7282955c416a4541763f81f6c51a7b9d009d5e
prokhorovd <github@prokhorov.dev>
2022-02-11 22:14:31 +0400

    feat: implement IconPicker using emoji-mart library

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import CreateRoomForm from './components/CreateRoomForm';
+import IconPicker from './components/IconPicker';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <IconPicker />
       <CreateRoomForm />
     </div>
   )
---
+++ client/src/components/IconPicker.tsx
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+import 'emoji-mart/css/emoji-mart.css'
+import {BaseEmoji, Picker, Emoji} from 'emoji-mart';
+import './IconPicker.css';
+
+function IconPicker() {
+  const [icon, setIcon] = useState<null | BaseEmoji>(null);
+  const [showPicker, setShowPicker] = useState(false);
+  const handleAddEmoji = (emoji: BaseEmoji) => {
+    setIcon(emoji);
+    setShowPicker(false);
+  }
+  return (
+    <div className='user-icon'>
+      <div className='user-icon__emoji' onClick={() => setShowPicker(!showPicker)}>
+        {icon === null ? <Emoji emoji=':question:' set='apple' size={24} /> : <Emoji emoji={icon} size={24} />}
+      </div>
+      {showPicker && <Picker set='apple' title='Select your user icon' style={{ position: 'absolute', bottom: '20px', right: '20px' }} onSelect={handleAddEmoji}/>}
+    </div>
+  );
+}
+
+export default IconPicker;
