9f5c44f1b95dde46df4f70a110ffbbb82832e2fc
prokhorovd <github@prokhorov.dev>
2022-02-25 15:32:21 +0400

    refactor: simplify timer component

--- client/src/components/Timer/Timer.tsx
+++ client/src/components/Timer/Timer.tsx
@@ -1,41 +1,34 @@
 import React, {FC, useEffect, useRef, useState} from 'react';
 import {Button} from '@mui/material';
-import {StyledTimerWrapper, StyledClockWrapper} from './styled';
+import {StyledClockWrapper, StyledTimerWrapper} from './styled';
+
+const START_TIME = 4;
 
 const Timer:FC = () => {
-  const [startValue, setStartValue] = useState(60);
-  const [timeLeft, setTimeLeft] = useState(startValue);
-  const [timerActive, setTimerActive] = useState(false);
+  const [timeLeft, setTimeLeft] = useState(START_TIME);
   const timerIdRef = useRef<null | ReturnType<typeof setInterval>>(null);
-
-  // Process countdown when timer is active and timeLeft > 0.
-  // Stop timer, set counter to startValue and do some action when done.
-  useEffect(() => {
-    timerIdRef.current = setInterval(() => {
-      timerActive && timeLeft > 0 && setTimeLeft(timeLeft => timeLeft - 1);
-    }, 1000);
-    // Clear interval, stop timer, set default timer value when count to zero
-    if (timeLeft === 0) {
-      clearInterval(timerIdRef.current);
-      setTimerActive(false);
-      setTimeLeft(startValue);
-      console.log('Time is out!')
-    } if (!timerActive) {
-      // reset timer if stop
-      setTimeLeft(startValue);
+  const startHandler = () => {
+    if (!timerIdRef.current) {
+      setTimeLeft(START_TIME);
+      timerIdRef.current = setInterval(() => {
+        setTimeLeft(timeLeft => timeLeft - 1)
+      }, 1000)
     }
-    // Clear interval when unmount
-    // @ts-ignore
-    return () => clearInterval(timerIdRef.current);
-  }, [timeLeft, timerActive]);
-
-  // display new startTime when new value received
+  }
+  const stopHandler = () => {
+    if (!!timerIdRef.current) {
+      clearInterval(timerIdRef.current);
+      timerIdRef.current = null;
+      console.log('Timer have stopped!');
+    }
+  }
   useEffect(() => {
-    setTimeLeft(startValue)
-  }, [startValue]);
+    if (timeLeft === 0) {
+      stopHandler();
+    }
+  }, [timeLeft]);
 
-  // transform seconds to {minutes: n, seconds: n}
-  function calculateTime(seconds: number) {
+  function convertTime(seconds: number) {
     const result = {minutes: '00', seconds: '00'}
     result.minutes = Math.floor(seconds / 60).toString();
     if (result.minutes.length === 1) {
@@ -47,18 +40,11 @@ const Timer:FC = () => {
     }
     return `${result.minutes} : ${result.seconds}`;
   }
-  // calculate time left: seconds => {minutes: n, seconds:n}
-  const calculatedTime = calculateTime(timeLeft)
-
   return (
     <StyledTimerWrapper>
-      <StyledClockWrapper>{calculatedTime}</StyledClockWrapper>
-      <div><Button onClick={() => setTimerActive(!timerActive)}>{timerActive ? 'Stop' : 'Start'}</Button></div>
-      <div>
-        <Button onClick={() => setStartValue(60)} disabled={timerActive}>Set to 1 min</Button>
-        <Button onClick={() => setStartValue(startValue + 30)} disabled={timerActive || startValue === 3570}>+30 sec</Button>
-        <Button onClick={() => setStartValue(startValue - 30)} disabled={timerActive ||  startValue === 30}>-30 sec</Button>
-      </div>
+      <StyledClockWrapper>{convertTime(timeLeft)}</StyledClockWrapper>
+      <Button onClick={startHandler} >start</Button>
+      <Button onClick={stopHandler} >stop</Button>
     </StyledTimerWrapper>
   );
 }
